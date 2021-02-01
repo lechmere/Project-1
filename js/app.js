6737ea67dff7748a25247a7cb7c8803a2da6c364
Natasha <natashasltaylor@gmail.com>
2021-02-01 21:42:33 +0000

    Project-1 no bugs

--- js/app.js
+++ js/app.js
@@ -32,16 +32,16 @@ const closeButtonOver = document.querySelector(".close-button-over")
 // restart
 function restart() {
   if (life === 0) {
-  cells[frog].classList.remove('frog-road')
-  cells[frog].classList.remove('frog-river')
-  frog = 126
-  cells[frog].classList.add('frog-floor')
-  life = 3
-  lives.innerHTML = `Lives: ${life}`
-  score = 0
-  highScore.innerHTML = `Hi-Score: ${score}`
-  toggleModalOver()
-}
+    cells[frog].classList.remove('frog-road')
+    cells[frog].classList.remove('frog-river')
+    frog = 126
+    cells[frog].classList.add('frog-floor')
+    life = 3
+    lives.innerHTML = `Lives: ${life}`
+    score = 0
+    highScore.innerHTML = `Hi-Score: ${score}`
+    toggleModalOver()
+  }
 }
 function toggleModalOver() {
   modalOver.classList.toggle("show-modal");
@@ -88,10 +88,10 @@ function squash() {
 }
 
 // to number the cells
-for (let i = 0; i < 132; i++) {
-  const div = cells[i]
-  div.innerHTML = i
-}
+// for (let i = 0; i < 132; i++) {
+//   const div = cells[i]
+//   div.innerHTML = i
+// }
 
 // road collision: replace frog, squash sound, remove life.
 function roadGameOver() {
@@ -110,14 +110,18 @@ function roadGameOver() {
 // river death: replace frog, sunk sound, remove life.
 function riverGameOver() {
   if (((cells[frog].classList.contains('frog-river')) && !(cells[frog].classList.contains('log')) && !(cells[frog].classList.contains('middle')) && !(cells[frog].classList.contains('back')) && !(cells[frog].classList.contains('front')) && !(cells[frog].classList.contains('turtle'))) || (cells[frog].classList.contains('sunk-turtle'))) {
-    sunk()
-    life -= 1
-    score -= 30
-    lives.innerHTML = `Lives: ${life}`
-    cells[frog].classList.remove('frog-river')
-    frog = 126
-    cells[frog].classList.add('frog-floor')
+    drowning('frog-river')
   }
+}
+
+function drowning(className) {
+  sunk()
+  life -= 1
+  score -= 30
+  lives.innerHTML = `Lives: ${life}`
+  cells[frog].classList.remove(className)
+  frog = 126
+  cells[frog].classList.add('frog-floor')
 }
 
 document.addEventListener('keypress', (event) => {
@@ -139,21 +143,21 @@ document.addEventListener('keypress', (event) => {
     moveFrog(MOVE_UP, 10, 'frog-floor', 'frog-river')
   } else if (key === 'w' && ((frog >= 33) && (frog <= 65))) {
     moveFrog(MOVE_UP, 10, 'frog-river', 'frog-river')
-  
+
   } else if ((key === 'a') && !(frog % width === 0) && (((frog >= 122) && (frog <= 131)) || ((frog >= 67) && (frog <= 76)))) {
     moveFrog(MOVE_LEFT, 10, 'frog-floor', 'frog-floor')
   } else if ((key === 'a') && !(frog % width === 0) && ((frog >= 78) && (frog <= 120))) {
     moveFrog(MOVE_LEFT, 10, 'frog-road', 'frog-road')
   } else if ((key === 'a') && !(frog % width === 0) && ((frog >= 23) && (frog <= 65))) {
     moveFrog(MOVE_LEFT, 10, 'frog-river', 'frog-river')
-  
+
   } else if (key === 'd' && !(frog % width === width - 1) && (((frog >= 121) && (frog <= 130)) || ((frog >= 66) && (frog <= 75)))) {
     moveFrog(MOVE_RIGHT, 10, 'frog-floor', 'frog-floor')
   } else if (key === 'd' && !(frog % width === width - 1) && ((frog >= 77) && (frog <= 119))) {
     moveFrog(MOVE_RIGHT, 10, 'frog-road', 'frog-road')
   } else if (key === 'd' && !(frog % width === width - 1) && ((frog >= 22) && (frog <= 64))) {
     moveFrog(MOVE_RIGHT, 10, 'frog-river', 'frog-river')
-  
+
   } else if (key === 's' && (frog >= (110)) && ((frog <= 120))) {
     moveFrog(MOVE_DOWN, 10, 'frog-road', 'frog-floor')
   } else if (key === 's' && ((frog >= 77) && (frog <= 109))) {
@@ -169,29 +173,43 @@ document.addEventListener('keypress', (event) => {
   riverGameOver()
   restart()
 })
-  
-function moveFrog(position, points, startClass, endClass) {
-    cells[frog].classList.remove(startClass)
-    frog -= position;
-    cells[frog].classList.add(endClass)
-    hop();
-    score += points;
-  }
+
+function moveFrog(distance, points, startClass, endClass) {
+  cells[frog].classList.remove(startClass)
+  frog -= distance;
+  cells[frog].classList.add(endClass)
+  updateFrogCell(frog);
+  hop();
+  score += points;
+}
 
 // Movement
-function objectMovement(object, start, end, interval, objectName, moveLeft, onObjectMove) {
- setInterval(() => {
+function objectMovement(object, start, end, interval, objectName, moveLeft, frogWithObject, frogWithObjectClass, frogWithObjectClassesRemove = []) {
+  setInterval(() => {
     object.forEach((objNum, i) => {
       if (objNum === start) {
         cells[objNum].classList.remove(objectName)
         object[i] = end
-        let resetPositionStart = moveLeft?objNum+10:objNum-10;
+        let resetPositionStart = moveLeft ? objNum + 10 : objNum - 10;
         cells[resetPositionStart].classList.add(objectName)
+        if (frogWithObject && frog === objNum) {
+          frogWithObjectClassesRemove.forEach((c) => cells[objNum].classList.remove(c))
+          drowning(frogWithObjectClass)
+        }
       } else {
+        cells[objNum].classList.remove(frogWithObjectClass)
         cells[objNum].classList.remove(objectName)
-        moveLeft ? object[i] -= 1 : object[i]+=1;
-        let resetPositionEnd = moveLeft?objNum-1:objNum+1;
+        moveLeft ? object[i] -= 1 : object[i] += 1;
+        let resetPositionEnd = moveLeft ? objNum - 1 : objNum + 1;
         cells[resetPositionEnd].classList.add(objectName)
+
+        if (frogWithObject && frog === objNum) {
+          console.log(frogWithObjectClass);
+          frogWithObjectClassesRemove.forEach((c) => cells[objNum].classList.remove(c))
+
+          frog = resetPositionEnd;
+          cells[frog].classList.add(frogWithObjectClass)
+        }
       }
       roadGameOver()
     })
@@ -201,41 +219,40 @@ objectMovement(car, 99, 109, 1000, 'car', true)
 objectMovement(racecar, 98, 88, 400, 'racecar', false)
 objectMovement(van, 77, 87, 1500, 'van', true)
 objectMovement(traffic, 120, 110, 150, 'traffic', false)
-objectMovement(log, 55, 65, 1000, 'log', true)
-objectMovement(front, 43, 33, 1000, 'front', false)
-objectMovement(middle, 43, 33, 1000, 'middle', false)
-objectMovement(back, 43, 33, 1000, 'back', false)
-objectMovement(turtle, 22, 32, 1000, 'turtle', true)
-objectMovement(turtle2, 44, 54, 600, 'turtle', true)
-
+objectMovement(log, 55, 65, 1000, 'log', true, true, 'logfrog', ['log', 'frog-river'])
+objectMovement(front, 33, 43, 1000, 'front', true,  true, 'frontfrog', ['front', 'frog-river', 'middlefrog', 'backfrog', 'frog-river',])
+objectMovement(middle, 33, 43, 1000, 'middle', true,  true, 'middlefrog', ['middle', 'frog-river', 'frontfrog', 'backfrog', 'frog-river',])
+objectMovement(back, 33, 43, 1000, 'back', true,  true, 'backfrog', ['back', 'frog-river', 'frontfrog', 'middlefrog', 'frog-river',])
+objectMovement(turtle, 32, 22, 1000, 'turtle', false, true, 'turtlefrog', ['turtle', 'frog-river'])
+objectMovement(turtle2,54, 44, 600, 'turtle', false, true, 'turtlefrog', ['turtle', 'frog-river'])
 
 setInterval(() => {
-  if (turtleChanger1 === 22) {
+  if (turtleChanger1 === 32) {
     cells[turtleChanger1].classList.remove('turtle')
     cells[turtleChanger1].classList.remove('sunk-turtle')
-    turtleChanger1 = 32
+    turtleChanger1 = 22
     cells[turtleChanger1].classList.add('turtle')
     cells[turtleChanger1].classList.add('sunk-turtle')
   } else {
     cells[turtleChanger1].classList.remove('turtle')
     cells[turtleChanger1].classList.remove('sunk-turtle')
-    turtleChanger1 -= 1
+    turtleChanger1 += 1
     cells[turtleChanger1].classList.add('turtle')
     cells[turtleChanger1].classList.add('sunk-turtle')
   }
 }, 1000)
 
 setInterval(() => {
-  if (turtleChanger2 === 22) {
+  if (turtleChanger2 === 32) {
     cells[turtleChanger2].classList.remove('turtle')
     cells[turtleChanger2].classList.remove('sunk-turtle')
-    turtleChanger2 = 32
+    turtleChanger2 = 22
     cells[turtleChanger2].classList.add('turtle')
     cells[turtleChanger2].classList.add('sunk-turtle')
   } else {
     cells[turtleChanger2].classList.remove('turtle')
     cells[turtleChanger2].classList.remove('sunk-turtle')
-    turtleChanger2 -= 1
+    turtleChanger2 += 1
     cells[turtleChanger2].classList.add('turtle')
     cells[turtleChanger2].classList.add('sunk-turtle')
   }
@@ -243,8 +260,8 @@ setInterval(() => {
 
 // sinking turtle
 function changeTurtle1() {
-    cells[turtleChanger1].classList.remove('turtle')
-    cells[turtleChanger1].classList.add('sunk-turtle')
+  cells[turtleChanger1].classList.remove('turtle')
+  cells[turtleChanger1].classList.add('sunk-turtle')
 }
 function changeTurtle2() {
   cells[turtleChanger2].classList.remove('turtle')
@@ -266,8 +283,8 @@ function changeTurtle2() {
 }());
 
 function changeTurtle1Back() {
-    cells[turtleChanger1].classList.remove('sunk-turtle')
-    cells[turtleChanger1].classList.add('turtle')
+  cells[turtleChanger1].classList.remove('sunk-turtle')
+  cells[turtleChanger1].classList.add('turtle')
 }
 function changeTurtle2Back() {
   cells[turtleChanger2].classList.remove('sunk-turtle')
